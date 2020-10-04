a8df02ac71991bb6488fee9c20047c755b1f2a3f
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-04 18:41:17 +0000

    lets it identify the animal

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,29 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println ("Does the animal have feathers?");
+    String featherAnswer = input.nextLine();
+    if(featherAnswer.equals("yes")){
+      
+      System.out.println("Does the animal swim?");
+      String animalSwim = input.nextLine();
+      if(animalSwim.equals("yes")){
+        System.out.println("This is a duck.");
+      }
+      else if(animalSwim.equals("no")){
+        System.out.println("This is a hen.");
+      }
+       else if(featherAnswer.equals("no")){
+        System.out.println("Does the animal have legs?");
+        String animalLegs = input.nextLine();
+        if(animalLegs.equals("yes")){
+          System.out.println("This is a lizard.");
+        }
+        else if(animalLegs.equals("no")){
+          System.out.println("This is a snake.");
+        }
+      }
+    }
   }
 }
